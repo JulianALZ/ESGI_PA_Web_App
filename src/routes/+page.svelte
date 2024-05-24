d824c4c6b9fb9dea9eafe1056c7134fd328ce040
JulianALZ <julian.alizay@gmail.com>
2024-05-24 22:04:59 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { browser } from '$app/environment';
+    // ...Your other imports
+
+    if (browser) { // to prevent error window is not defined, because it's SSR
+        window.location.href = '/login';
+    }
+
+</script>
+
+
---
+++ src/routes/homepage/+page.svelte
@@ -0,0 +1,47 @@
+<script>
+	import "./homepage.css";
+
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const token = localStorage.getItem('token');
+
+		if (!token) {
+			window.location.href = '/login';
+			return;
+		}
+
+		try {
+			const decodedToken = jwt_decode(token);
+			const currentTime = Date.now() / 1000;
+			if(decodedToken.exp < currentTime){
+				window.location.href = '/login';
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	});
+
+
+	// Importez tous les composants ici
+	import Header from './components/Header.svelte';
+	import BalanceSummary from './components/BalanceSummary.svelte';
+	import LineChart from './components/LineChart.svelte';
+	import ActionButtons from './components/ActionButtons.svelte';
+	import CryptoList from './components/CryptoList.svelte';
+
+
+	import "./homepage.css";
+</script>
+
+<div class="gradient-custom">
+	<Header />
+	<BalanceSummary />
+	<LineChart />
+	<ActionButtons />
+	<CryptoList />
+</div>
+
+
+
+
